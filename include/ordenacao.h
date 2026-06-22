03c0ca93c6ed9a387fddae5f02c6a5994dea62c7
Gabriel Fatureto <gabriel.fatureto.freitas@uel.br>
2026-06-22 16:28:10 -0400

    Tinha esquecido de dois dos algoritmos pedidos, mas ja adicionei

--- include/ordenacao.h
+++ include/ordenacao.h
@@ -11,11 +11,13 @@
  */
 typedef int (*ComparaForma)(Forma a, Forma b);
 
-/* Algoritmos de ordenação O(n^2) */
+/* Algoritmos de ordenação O(n^2) // Simples */
 void bubble_sort(Forma vetor[], int n, ComparaForma cmp);
 void selection_sort(Forma vetor[], int n, ComparaForma cmp);
+void insertion_sort(Forma vetor[], int n, ComparaForma cmp);
 
-/* Algoritmos de ordenação O(n log n) */
+/* Algoritmos de ordenação O(n log n) ou intermediários */
+void shell_sort(Forma vetor[], int n, ComparaForma cmp);
 void quick_sort(Forma vetor[], int inicio, int fim, ComparaForma cmp);
 void merge_sort(Forma vetor[], int inicio, int fim, ComparaForma cmp);
 
